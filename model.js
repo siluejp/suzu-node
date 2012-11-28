2084454572de9d46a9783f172ede83bd643d635b
shinji.suzuki <ssuzuki@jobhub.jp>
2012-11-29 03:15:33 +0900

    first

---
+++ model.js
@@ -0,0 +1,15 @@
+const MONGO_URL = process.env.MONGOHQ_URL;
+
+var mongoose = require('mongoose');
+var db = mongoose.connect(MONGO_URL);
+
+function validator(v) {
+  return v.length > 0;
+}
+
+var Post = new mongoose.Schema({
+    text   : { type: String, validate: [validator, "Empty Error"] }
+  , created: { type: Date, default: Date.now }
+});
+
+exports.Post = db.model('Post', Post);
